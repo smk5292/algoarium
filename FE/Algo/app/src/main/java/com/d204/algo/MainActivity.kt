1f4f9d6d51151db25603d05ce470eb8fdcbc5221
kieanu <kieanupark@gmail.com>
2023-09-10 19:47:55 +0900

    Feat: 새 시즌 랭크 업 애니메이션 추가

--- FE/Algo/app/src/main/java/com/d204/algo/MainActivity.kt
+++ FE/Algo/app/src/main/java/com/d204/algo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.d204.algo
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
@@ -19,6 +20,7 @@ import com.d204.algo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+
 private const val TAG = "MainActivity"
 
 @AndroidEntryPoint
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var bubbleTransition: GifDrawable
+    private lateinit var rankUpEffect: GifDrawable
 
     @Inject
     lateinit var glide: RequestManager
@@ -33,9 +36,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setAnimation()
+//        setAnimation() // 새 시즌 시작이 아니면 바로 호출
+        setRankAnimation() // 새 시즌으로 랭크업 해야하면 호출 후 클릭 시 setAnimation 호출
         setContentView(binding.root)
         setupNavHost()
+//        카카오 앱 개발자 페이지에 등록하기
 //        var keyHash = Utility.getKeyHash(this)
 //        Log.d(TAG, "onCreate: $keyHash")
 //        showSnackBar(binding.root, "ㅎㅇ")
@@ -54,7 +59,7 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationStart(animation: Animation?) {}
 
             override fun onAnimationEnd(animation: Animation?) {
-                binding.activityMainUpperBg.visibility = View.GONE // 뷰를 숨기거나
+                binding.activityMainUpperBg.visibility = View.GONE // 뷰를 숨기기
             }
 
             override fun onAnimationRepeat(animation: Animation?) {}
@@ -65,20 +70,20 @@ class MainActivity : AppCompatActivity() {
     private fun setAnimation() {
         glide.asGif()
             .load(R.drawable.bubble_transition)
-            .listener(object : RequestListener<com.bumptech.glide.load.resource.gif.GifDrawable> {
+            .listener(object : RequestListener<GifDrawable> {
                 override fun onLoadFailed(
                     e: GlideException?,
                     model: Any?,
-                    target: Target<com.bumptech.glide.load.resource.gif.GifDrawable>?,
+                    target: Target<GifDrawable>?,
                     isFirstResource: Boolean,
                 ): Boolean {
                     return false
                 }
 
                 override fun onResourceReady(
-                    resource: com.bumptech.glide.load.resource.gif.GifDrawable?,
+                    resource: GifDrawable?,
                     model: Any?,
-                    target: com.bumptech.glide.request.target.Target<com.bumptech.glide.load.resource.gif.GifDrawable>?,
+                    target: Target<GifDrawable>?,
                     dataSource: DataSource?,
                     isFirstResource: Boolean,
                 ): Boolean {
@@ -97,6 +102,54 @@ class MainActivity : AppCompatActivity() {
                 }
             })
             .into(binding.transitionAnim)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setRankAnimation() {
+        val animator = ObjectAnimator.ofFloat(binding.activityMainTierBefore, "rotationY", 0f, 10800f)
+        animator.duration = 5000
+        animator.start()
+
+        glide.asGif()
+            .load(R.drawable.tier_up_light_transparent)
+            .listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    isFirstResource: Boolean,
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: GifDrawable?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean,
+                ): Boolean {
+                    rankUpEffect = resource!!
+                    rankUpEffect.setLoopCount(1)
+
+                    rankUpEffect.registerAnimationCallback(object :
+                        Animatable2Compat.AnimationCallback() {
+                        override fun onAnimationEnd(drawable: Drawable?) {
+                            binding.apply{
+                                activityMainRankUpEffect.visibility = View.GONE
+                                activityMainTierBefore.visibility = View.GONE
+                                activityMainRankUp.setOnClickListener {
+                                    it.visibility = View.GONE
+                                    setAnimation()
+                                }
+                            }
+                        }
+                    })
+
+                    return false
+                }
+            })
+            .into(binding.activityMainRankUpEffect)
     }
 
     fun callTransition() {
