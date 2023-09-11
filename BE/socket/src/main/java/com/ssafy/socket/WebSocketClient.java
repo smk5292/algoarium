862ac89142565a892dbfd1bb8a029a5c43e83959
KimHyeongJin <gudwls9966@naver.com>
2023-09-11 17:59:36 +0900

    1

--- BE/socket/src/main/java/com/ssafy/socket/WebSocketClient.java
+++ BE/socket/src/main/java/com/ssafy/socket/WebSocketClient.java
@@ -1,6 +1,9 @@
 package com.ssafy.socket;
 
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
@@ -8,6 +11,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.InputStream;
 
 public class WebSocketClient {
 
@@ -17,21 +21,30 @@ public class WebSocketClient {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+
+                // 백그라운드 이미지 추가
+                ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/see.jpg"));
+                Image backgroundImage = backgroundIcon.getImage().getScaledInstance(375, 595, Image.SCALE_SMOOTH);
+                ImageIcon smallBackgroundIcon = new ImageIcon(backgroundImage);
+                JLabel backgroundLabel = new JLabel(smallBackgroundIcon);
+
                 // UI 요소 생성
                 ImageIcon starIcon = new ImageIcon(getClass().getResource("/star.png")); // star.png 이미지 추가
                 Image starImage = starIcon.getImage().getScaledInstance(starIcon.getIconWidth() / 2, starIcon.getIconHeight() / 2, Image.SCALE_SMOOTH);
                 ImageIcon smallStarIcon = new ImageIcon(starImage);
                 JLabel starLabel = new JLabel(smallStarIcon); // star.png 이미지 추가
 
-                JTextField channelIdField = new JTextField(23); // 채널 ID 입력 필드
+                JTextField channelIdField = new JTextField(13); // 채널 ID 입력 필드
                 Dimension fieldPreferredSize = channelIdField.getPreferredSize();
                 fieldPreferredSize.height *= 1.9; // 상하 길이를 두 배로 늘리기
                 channelIdField.setPreferredSize(fieldPreferredSize);
+                channelIdField.setFont(new Font("Arial", Font.PLAIN, 18)); // 폰트 설정
 
-                JButton connectButton = new JButton("로그인"); // 연결 버튼
+                JButton connectButton = new JButton("Connect"); // 연결 버튼
                 connectButton.setMaximumSize(channelIdField.getPreferredSize());
                 connectButton.setMinimumSize(channelIdField.getPreferredSize());
                 connectButton.setPreferredSize(channelIdField.getPreferredSize());
+
 
 
                 // 버튼의 크기를 1.1배로 늘리기
@@ -39,11 +52,9 @@ public class WebSocketClient {
                 buttonPreferredSize.height *= 20;
                 connectButton.setPreferredSize(buttonPreferredSize);
 
-
-
                 // 연결 버튼 이전에 채널 ID 입력 필드 추가
                 JPanel panel = new JPanel(); // 패널 생성
-
+                panel.setOpaque(false); // 패널을 투명하게 설정
 
                 GroupLayout layout = new GroupLayout(panel);
                 panel.setLayout(layout);
@@ -60,7 +71,9 @@ public class WebSocketClient {
 
                 layout.setVerticalGroup(
                         layout.createSequentialGroup()
+                                .addGap(50) // 수직 간격을 둬서 아래로 내림
                                 .addComponent(starLabel)
+                                .addGap(20) // 수직 간격을 둬서 아래로 내림
                                 .addComponent(channelIdField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                                 .addComponent(connectButton)
                 );
@@ -75,18 +88,41 @@ public class WebSocketClient {
 
                         String channelId = channelIdField.getText(); // 입력한 채널 ID 가져오기
                         MySessionHandler sessionHandler = new MySessionHandler(channelId); // 세션 핸들러 생성
-                        stompClient.connect(url, sessionHandler); // 서버에 연결
+
+                        // 연결 중 버튼으로 변경
+                        connectButton.setEnabled(false);
+                        connectButton.setText("Success");
+
+                        stompClient.connect(url, sessionHandler, new StompSessionHandlerAdapter() {
+                            @Override
+                            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                                // 연결 완료되면 '로그인'으로 버튼 텍스트 변경
+                                SwingUtilities.invokeLater(() -> {
+                                    connectButton.setEnabled(true);
+                                    connectButton.setText("Connect");
+                                });
+
+                                // 연결 완료 후 필요한 작업 수행
+                                // ...
+                            }
+                        }); // 서버에 연결
                     }
                 });
 
+
                 JFrame frame = new JFrame("Algoarium"); // 프레임 생성
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프레임 닫기 버튼 동작 설정
+
+                // 백그라운드 이미지 추가
+                frame.setContentPane(new JLabel(backgroundIcon));
+                frame.setLayout(new FlowLayout());
+
+                frame.setIconImage(starImage); // 프레임 아이콘 설정
+
                 frame.getContentPane().add(panel); // 패널을 프레임에 추가
                 frame.pack(); // 크기 자동 조절
                 frame.setVisible(true); // 화면에 표시
                 frame.setLocationRelativeTo(null); // 화면 중앙에 표시
-                ImageIcon icon = new ImageIcon(getClass().getResource("/star.png")); // resources 폴더 안에 있는 이미지 사용
-                frame.setIconImage(icon.getImage()); // 프레임 아이콘 설정
 
                 frame.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
