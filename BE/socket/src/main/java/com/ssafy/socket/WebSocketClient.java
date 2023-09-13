4af4d09a662e7a69d8dae66c77095568b01d624f
KimHyeongJin <gudwls9966@naver.com>
2023-09-13 17:50:09 +0900

    Feat : 주석추가

--- BE/socket/src/main/java/com/ssafy/socket/WebSocketClient.java
+++ BE/socket/src/main/java/com/ssafy/socket/WebSocketClient.java
@@ -19,73 +19,70 @@ import java.nio.file.StandardOpenOption;
 
 public class WebSocketClient {
 
-    private WebSocketStompClient stompClient;
-    private static final String LOCK_FILE = "websocket.lock";
-    private static Path lockFilePath = Paths.get(LOCK_FILE);
+    private WebSocketStompClient stompClient; // WebSocket 클라이언트
 
+    // 락 파일 경로 및 객체
+    private static final String LOCK_FILE = "websocket.lock";  // 상수 LOCK_FILE에 "websocket.lock" 파일명을 저장합니다.
+    private static Path lockFilePath = Paths.get(LOCK_FILE);  // lockFilePath에 "websocket.lock" 파일의 경로를 설정합니다.
+
+    // 애플리케이션이 이미 실행 중인지 확인하는 메서드입니다.
+    private static boolean isAppAlreadyRunning() {
+        try {
+            FileChannel channel = FileChannel.open(lockFilePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            // "websocket.lock" 파일을 생성하거나 엽니다.
+
+            FileLock lock = channel.tryLock();  // 파일 락을 시도합니다.
+            if (lock == null) {  // 락이 획득되지 않았다면
+                channel.close();  // 파일 채널을 닫습니다.
+                return true;  // 이미 애플리케이션이 실행 중이라고 판단합니다.
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  // 입출력 예외가 발생한 경우 에러를 출력합니다.
+        }
+        return false;  // 애플리케이션이 실행 중이 아닌 것으로 판단합니다.
+    }
+
+
+    // 클라이언트 애플리케이션을 시작하는 메서드
     public void start() {
         if (isAppAlreadyRunning()) {
             System.out.println("The application is already running.");
             return;
         }
+
+        // UI 생성을 별도의 스레드에서 처리
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
-                // 백그라운드 이미지 추가
-                ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/see.jpg"));
-                Image backgroundImage = backgroundIcon.getImage().getScaledInstance(375, 595, Image.SCALE_SMOOTH);
-                ImageIcon smallBackgroundIcon = new ImageIcon(backgroundImage);
-                JLabel backgroundLabel = new JLabel(smallBackgroundIcon);
-
-                // UI 요소 생성
-                ImageIcon starIcon = new ImageIcon(getClass().getResource("/star.png")); // star.png 이미지 추가
+                // 불가사리 이미지 추가
+                ImageIcon starIcon = new ImageIcon(getClass().getResource("/star.png"));
                 Image starImage = starIcon.getImage().getScaledInstance(starIcon.getIconWidth() / 2, starIcon.getIconHeight() / 2, Image.SCALE_SMOOTH);
                 ImageIcon smallStarIcon = new ImageIcon(starImage);
-                JLabel starLabel = new JLabel(smallStarIcon); // star.png 이미지 추가
+                JLabel starLabel = new JLabel(smallStarIcon);
 
-
-                JTextField channelIdField = new JTextField(13); // 채널 ID 입력 필드
+                // 채널 ID 입력 필드 추가
+                JTextField channelIdField = new JTextField(13);
                 Dimension fieldPreferredSize = channelIdField.getPreferredSize();
                 fieldPreferredSize.height *= 1.9; // 상하 길이를 두 배로 늘리기
                 channelIdField.setPreferredSize(fieldPreferredSize);
                 channelIdField.setFont(new Font("Arial", Font.PLAIN, 18)); // 폰트 설정
-
-
+                channelIdField.setHorizontalAlignment(JTextField.CENTER); // 채널 ID Field에 대한 텍스트 가운데 정렬 설정
 
                 JButton connectButton = new JButton("Connect"); // 연결 버튼
                 connectButton.setMaximumSize(channelIdField.getPreferredSize());
                 connectButton.setMinimumSize(channelIdField.getPreferredSize());
                 connectButton.setPreferredSize(channelIdField.getPreferredSize());
 
+                Dimension buttonPreferredSize = connectButton.getPreferredSize();
+                buttonPreferredSize.height *= 1.1; // 버튼의 크기를 1.1배로 늘리기
+                connectButton.setPreferredSize(buttonPreferredSize);
+
                 JButton exitButton = new JButton("Exit"); // 프로세스 종료 버튼
                 exitButton.setMaximumSize(channelIdField.getPreferredSize());
                 exitButton.setMinimumSize(channelIdField.getPreferredSize());
                 exitButton.setPreferredSize(channelIdField.getPreferredSize());
 
-// exitButton을 눌렀을 때의 동작 설정
-                exitButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        // 프로그램 종료 시 WebSocket 클라이언트 정지
-                        if (stompClient != null) {
-                            stompClient.stop();
-                        }
-                        System.exit(0); // 시스템 종료
-                    }
-                });
-
-                // channelIdField에 대한 텍스트 가운데 정렬 설정
-                channelIdField.setHorizontalAlignment(JTextField.CENTER);
-
-
-
-                // 버튼의 크기를 1.1배로 늘리기
-                Dimension buttonPreferredSize = connectButton.getPreferredSize();
-                buttonPreferredSize.height *= 20;
-                connectButton.setPreferredSize(buttonPreferredSize);
-
-                // 연결 버튼 이전에 채널 ID 입력 필드 추가
+                // 채팅 연결 버튼 이전에 채널 ID 입력 필드 추가
                 JPanel panel = new JPanel(); // 패널 생성
                 panel.setOpaque(false); // 패널을 투명하게 설정
 
@@ -97,34 +94,31 @@ public class WebSocketClient {
 
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                                .addComponent(starLabel)
-                                .addComponent(channelIdField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addComponent(connectButton)
-                                .addComponent(exitButton)
-
+                                .addComponent(starLabel)  // 불가사리 이미지
+                                .addComponent(channelIdField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)  // 채널 ID 입력 필드
+                                .addComponent(connectButton)  // Connect 버튼
+                                .addComponent(exitButton)  // Exit 버튼
                 );
 
                 layout.setVerticalGroup(
                         layout.createSequentialGroup()
                                 .addGap(50)
-                                .addComponent(starLabel)
+                                .addComponent(starLabel)  // 불가사리 이미지
                                 .addGap(20)
-                                .addComponent(channelIdField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addComponent(connectButton)
+                                .addComponent(channelIdField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)  // 채널 ID 입력 필드
+                                .addComponent(connectButton)  // Connect 버튼
                                 .addGap(40)
-                                .addComponent(exitButton)
-                                .addComponent(exitButton)
-
+                                .addComponent(exitButton)  // Exit 버튼
                 );
+
                 JFrame frame = new JFrame("Algoarium"); // 프레임 생성
                 frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); // 프레임 닫기 버튼 동작 설정
-
 
                 // 시스템 트레이 설정
                 if (SystemTray.isSupported()) {
                     SystemTray tray = SystemTray.getSystemTray();
-//                    Image image = Toolkit.getDefaultToolkit().getImage("/arrow.png"); // 아이콘 이미지 경로 지정
 
+                    // Show 옵션 클릭 시 실행되는 액션
                     ActionListener showAction = new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
                             frame.setVisible(true);
@@ -132,11 +126,13 @@ public class WebSocketClient {
                         }
                     };
 
+                    // 시스템 트레이 팝업 메뉴
                     PopupMenu popup = new PopupMenu();
                     MenuItem showItem = new MenuItem("Show");
                     showItem.addActionListener(showAction);
                     popup.add(showItem);
 
+                    // Exit 옵션 클릭 시 애플리케이션 종료
                     MenuItem exitItem = new MenuItem("Exit");
                     exitItem.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
@@ -147,8 +143,8 @@ public class WebSocketClient {
                         }
                     });
 
-
                     popup.add(exitItem);
+
                     ImageIcon icon = new ImageIcon(getClass().getResource("/star2.png"));
                     Image image = icon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
                     TrayIcon trayIcon = new TrayIcon(image, "Algoarium", popup);
@@ -162,12 +158,23 @@ public class WebSocketClient {
                         }
                     });
 
-
                     try {
                         tray.add(trayIcon);
                     } catch (AWTException e) {
                         e.printStackTrace();
                     }
+
+                    // Exit 버튼 클릭 시 애플리케이션 종료
+                    exitButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            // 프로그램 종료 시 WebSocket 클라이언트 정지
+                            if (stompClient != null) {
+                                stompClient.stop();
+                            }
+                            System.exit(0); // 시스템 종료
+                        }
+                    });
 
                     // 프레임을 아이콘화할 때 처리할 이벤트
                     frame.addWindowListener(new WindowAdapter() {
@@ -184,12 +191,10 @@ public class WebSocketClient {
                             // 창이 닫히는 이벤트를 처리합니다.
                             frame.setVisible(false);
                         }
-
-
                     });
                 }
 
-                // Connect 버튼 ActionListener 내부에서의 코드
+                // Connect 버튼 ActionListener
                 connectButton.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -232,11 +237,8 @@ public class WebSocketClient {
                     }
                 });
 
-
-
-
-
                 // 백그라운드 이미지 추가
+                ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/see.jpg"));
                 frame.setContentPane(new JLabel(backgroundIcon));
                 frame.setLayout(new FlowLayout());
 
@@ -260,23 +262,11 @@ public class WebSocketClient {
                         frame.setVisible(false);
                     }
                 });
-
             }
         });
     }
-    private static boolean isAppAlreadyRunning() {
-        try {
-            FileChannel channel = FileChannel.open(lockFilePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-            FileLock lock = channel.tryLock();
-            if (lock == null) {
-                channel.close();
-                return true;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
+
+    // 애플리케이션 메인 메서드
     public static void main(String[] args) {
         WebSocketClient client = new WebSocketClient(); // WebSocketClient 객체 생성
         client.start(); // 클라이언트 시작
