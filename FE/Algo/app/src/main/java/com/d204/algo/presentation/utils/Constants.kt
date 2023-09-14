b3218c583e403184ee01ff11669b00889b158191
kieanupark <kieanupark@gmail.com>
2023-09-14 16:59:17 +0900

    Feat: 웹소켓 연결 기능 추가

--- FE/Algo/app/src/main/java/com/d204/algo/presentation/utils/Constants.kt
+++ FE/Algo/app/src/main/java/com/d204/algo/presentation/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.d204.algo.presentation.utils
 object Constants {
     const val DEBUG = true
     const val BASE_URL = "http://192.168.100.65:8080/"
-    const val SOCKET_URL = "ws://172.20.14.160:8080/chat/chatting/websocket" // ws://[도메인]/[엔드포인트]/websocket
+    const val SOCKET_URL = "ws://192.168.100.169:8080/websocket" // ws://[도메인]/[엔드포인트]/websocket
 }
