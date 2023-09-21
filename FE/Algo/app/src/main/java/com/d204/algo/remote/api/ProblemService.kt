069252061a7b5bdb449d9c17ddf600302dfda838
kieanupark <kieanupark@gmail.com>
2023-09-21 15:35:32 +0900

    Feat: ProblemService 수정

--- FE/Algo/app/src/main/java/com/d204/algo/remote/api/ProblemService.kt
+++ FE/Algo/app/src/main/java/com/d204/algo/remote/api/ProblemService.kt
@@ -1,14 +1,22 @@
 package com.d204.algo.remote.api
 
 import com.d204.algo.remote.model.ProblemModel
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ProblemService {
     @GET("api/problem")
-    suspend fun getProblems(): Response<List<ProblemModel>>
+    suspend fun getProblems(): List<ProblemModel>
 
     @GET("api/problem/{id}")
-    suspend fun getProblem(@Path("id") id: Long): Response<ProblemModel>
+    suspend fun getProblem(@Path("id") id: Long): ProblemModel
+
+    @GET("recommend/strong/{userId}")
+    suspend fun getStrongProblems(@Path("userId") userId: Long): List<ProblemModel>
+
+    @GET("recommend/weak/{userId}")
+    suspend fun getWeakProblems(@Path("userId") userId: Long): List<ProblemModel>
+
+    @GET("recommend/similar/{userId}")
+    suspend fun getSimilarProblems(@Path("userId") userId: Long): List<ProblemModel>
 }
