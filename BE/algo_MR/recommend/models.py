0c287212222d514954e45821ae227360aff5187c
DESKTOP-DOGVPUB\SSAFY <smk921@naver.com>
2023-09-22 09:55:11 +0900

    Feat: 강,약점문제추천(미완성)

--- BE/algo_MR/recommend/models.py
+++ BE/algo_MR/recommend/models.py
@@ -1,38 +1,68 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-# Create your models here.
-class BeakjoonUser(models.Model):
-    bj_id = models.CharField(max_length=100)
-    solved_count = models.IntegerField()
-    bj_class = models.IntegerField()
-    tier = models.IntegerField()
-    rating =models.IntegerField()
-    rating_by_problem_sum = models.IntegerField()
-    rating_by_class = models.IntegerField()
-    rating_by_solved = models.IntegerField()
-    rank = models.IntegerField()
-
-    class Meta:
-        db_table = 'beakjoon_user'
 
 
 class Problem(models.Model):
-    title = models.CharField(max_length=100)
+    problem_id = models.AutoField(primary_key=True)
     problem_level = models.IntegerField()
-    sovled_user_count = models.IntegerField()
-    problem_point = models.IntegerField()
-    users = models.ManyToManyField('BeakJoonUser')
+    problem_number = models.IntegerField()
+    problem_tag = models.CharField(max_length=100)
+    solved_user_count = models.IntegerField()
+    title = models.CharField(max_length=100)
 
     class Meta:
+        managed = False
         db_table = 'problem'
 
-class Tag(models.Model):
-    tag_name = models.CharField(max_length=255)
-    problems = models.ManyToManyField('Problem')
-    
+
+class ProblemLike(models.Model):
+    problem_like_id = models.AutoField(primary_key=True)
+    like_type = models.TextField()  # This field type is a guess.
+    memo = models.TextField()
+    problem = models.ForeignKey(Problem, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
+
     class Meta:
+        managed = False
+        db_table = 'problem_like'
+
+
+class ProblemTag(models.Model):
+    problem_tag_id = models.AutoField(primary_key=True)
+    problem = models.ForeignKey(Problem, models.DO_NOTHING, blank=True, null=True)
+    tag = models.ForeignKey('Tag', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'problem_tag'
+
+
+class Tag(models.Model):
+    tag_id = models.BigAutoField(primary_key=True)
+    tag_name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
         db_table = 'tag'
 
+
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    kakao_id = models.CharField(max_length=100)
+    kakao_nickname = models.CharField(max_length=50)
+    pre_tier = models.CharField(max_length=20)
+    profile_image = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
 
 class TagCorrelation(models.Model):
     tag1 = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tag1_correlations')
@@ -41,6 +71,3 @@ class TagCorrelation(models.Model):
 
     class Meta:
         db_table = 'tag_correlation'
-
-
-
--- BE/algo_MR/recommend/serializers.py
+++ BE/algo_MR/recommend/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import BeakjoonUser, Problem, Tag
+from .models import Problem, Tag
 
-class BeakjoonUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BeakjoonUser
-        fields = ("__all__")
-        #fields = (bj_id,solved_count,tier)
+# class BeakjoonUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BeakjoonUser
+#         fields = ("__all__")
+#         #fields = (bj_id,solved_count,tier)
 
 class ProblemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +16,7 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ("__all__")
+
+class ProblemTagSerializer(serializers.Serializer):
+    problem_id = serializers.IntegerField()
+    tag_ids = serializers.ListField(child=serializers.IntegerField())
