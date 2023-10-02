eccd0f2f4be4dbe0df264ff192201f532e6cd21a
hyunjin.kim <hyunjin.k1994@gmail.com>
2023-10-02 19:18:03 +0900

    Feat : EC2 DB server 열기

--- BE/algoarium/src/main/java/com/ssafy/algoarium/KakaoLogin/KakaoLoginController.java
+++ BE/algoarium/src/main/java/com/ssafy/algoarium/KakaoLogin/KakaoLoginController.java
@@ -30,7 +30,7 @@ public class KakaoLoginController {
 	//접속 창 이동
 	//https://kauth.kakao.com/oauth/authorize
 	//?response_type=code&client_id=1e6e787538b273266b9e8054397aec13
-	//&redirect_uri=http://localhost:8090/auth/kakao/callback
+	//&redirect_uri=http://j9d204.p.ssafy.io:8090/auth/kakao/callback
 	@Autowired
 	KakaoLoginService kakaoLoginService;
 
@@ -42,7 +42,7 @@ public class KakaoLoginController {
 		RedirectView redirectView = new RedirectView();
 		redirectView.setUrl("https://kauth.kakao.com/oauth/authorize"
 			+ "?response_type=code&client_id=1e6e787538b273266b9e8054397aec13"
-			+ "&redirect_uri=http://localhost:8090/auth/kakao/callback");
+			+ "&redirect_uri=http://j9d204.p.ssafy.io:8090/auth/kakao/callback");
 
 		return redirectView;
 	}
